f2db2888e1b3f4f091da5915ca44c49125ebca45
Dharshini2120 <dharshinitharika2003@gmail.com>
2026-04-19 21:20:58 +0530

    Delete views.py

--- views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from .models import Project
-
-def home(request):
-    # This pulls all projects you added in the Admin panel
-    projects = Project.objects.all()
-    return render(request, 'index.html', {'projects': projects})
